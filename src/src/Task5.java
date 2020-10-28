0f69a8845242ede130a669a65b5f810e67984e9b
IDoroftei <irina.doroftei@endava.com>
2020-10-28 16:44:12 +0200

    Task7 point 3
     Updated add methods

--- src/src/Task5.java
+++ src/src/Task5.java
@@ -1,4 +1,10 @@
 public class Task5 {
+    public void method1(){
+        System.out.println("Good day");
+    }
+    public int method2(int numar){
+        return numar;
+    }
     public static void main(String[] args) {
         System.out.println("Hello,hello");
     }
