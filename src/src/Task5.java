edaa4dab6ed9a11e9a02280a070b83a46aacdc6b
IDoroftei <irina.doroftei@endava.com>
2020-10-28 17:02:18 +0200

    Task7 point 7
    Added methods commit Branch2

--- src/src/Task5.java
+++ src/src/Task5.java
@@ -1,10 +1,20 @@
 public class Task5 {
-    public void method1(){
+    public void method1() {
         System.out.println("Good day");
     }
-    public int method2(int numar){
+
+    public int method2(int numar) {
         return numar;
     }
+
+    public void method4() {
+        System.out.println("Good by");
+    }
+
+    public void method5() {
+        System.out.println("Good morning");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello,hello");
     }
